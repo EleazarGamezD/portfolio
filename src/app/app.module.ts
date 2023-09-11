ae572064439eb70eb5e28f4fab095e967f58fd7b
Eleazar Gamez <82742317+froitzent@users.noreply.github.com>
2023-09-11 15:03:29 -0500

    creacion del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
